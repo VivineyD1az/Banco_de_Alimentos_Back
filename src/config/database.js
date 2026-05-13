db1b31e42623cd8ce082da9105df0523f979ebe7
Viviney <viviney.diazm@unac.edu.co>
2026-05-13 09:19:45 -0500

    Agregados modelos y middleware de autenticacion

---
+++ src/config/database.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const esAiven = process.env.DB_HOST && process.env.DB_HOST.includes('aivencloud.com');
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    logging: false,
+    dialectOptions: esAiven ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    } : {}
+  }
+);
+
+module.exports = { sequelize };
---
+++ src/models/index.js
@@ -0,0 +1,27 @@
+const Usuario = require('./Usuario');
+const Donante = require('./Donante');
+const Producto = require('./Producto');
+const { sequelize } = require('../config/database');
+const { DataTypes } = require('sequelize');
+
+const Donacion = sequelize.define('Donacion', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  usuario_id: { type: DataTypes.INTEGER },
+  donante_id: { type: DataTypes.INTEGER },
+  cantidad: { type: DataTypes.FLOAT },
+  fecha: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+}, { tableName: 'donaciones', timestamps: true });
+
+const DonacionProducto = sequelize.define('DonacionProducto', {
+  donacion_id: { type: DataTypes.INTEGER },
+  producto_id: { type: DataTypes.INTEGER }
+}, { tableName: 'donacion_productos', timestamps: false });
+
+Usuario.hasMany(Donacion, { foreignKey: 'usuario_id' });
+Donacion.belongsTo(Usuario, { foreignKey: 'usuario_id' });
+Donante.hasMany(Donacion, { foreignKey: 'donante_id' });
+Donacion.belongsTo(Donante, { foreignKey: 'donante_id' });
+Donacion.belongsToMany(Producto, { through: DonacionProducto, foreignKey: 'donacion_id' });
+Producto.belongsToMany(Donacion, { through: DonacionProducto, foreignKey: 'producto_id' });
+
+module.exports = { sequelize, Usuario, Donante, Producto, Donacion, DonacionProducto };
